0ba9962d57aba367b13c2daf392af9cc0437ffe2
adrija-12 <138452419+adrija-12@users.noreply.github.com>
2023-07-03 15:09:36 +0530

    ece@python-adrija

---
+++ loops.py
@@ -0,0 +1,6 @@
+class Test:
+    def main():
+        a=str(input("Enter your Name -> "))
+        b=str(input("Enter your Age -> "))
+        print("Your name is ",a," and your age is ",b)
+Test.main()
